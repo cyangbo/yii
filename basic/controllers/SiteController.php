fd2efd98a4ff4d8c188b246014c22685467b7a10
Administrator <Administrator@192.168.1.178>
2015-09-09 11:33:57 +0800

    v0.0.3 使用表单(未通过)

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -106,4 +106,28 @@ class SiteController extends Controller
     public function actionSay($message = 'Hello'){
     	return $this->render('say',['message' => $message]);
     }
+    
+    
+    
+    //http://yii.com/index.php?r=site/entry
+    public function actionEntry()
+    {
+    	$model = new EntryForm;
+    
+    	if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+    		// 验证 $model 收到的数据
+    
+    		// 做些有意义的事 ...
+    
+    		return $this->render('entry-confirm', ['model' => $model]);
+    	} else {
+    		// 无论是初始化显示还是数据验证错误
+    		return $this->render('entry', ['model' => $model]);
+    	}
+    }
+    
+    
+    
+    
+    
 }

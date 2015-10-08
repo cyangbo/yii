b861460bdb28cbf041e4ac82233af00ead0fd9ac
cyangbo <Administrator@PC-201506290051>
2015-10-09 00:20:45 +0800

    在视图中加载另外一个视图文件

--- basic/views/hello/about.php
+++ basic/views/hello/about.php
@@ -1,1 +1,5 @@
 hello about,这是hello/about.php的内容
+<br>
+下面是从hello/index.php传递过来的参数:
+<br>
+<?=$v_hello ?>
--- basic/views/hello/index.php
+++ basic/views/hello/index.php
@@ -1,1 +1,6 @@
 hello index,这是index页面的内容:hello/index.php
+<?php 
+	//在视图中加载另外一个视图文件
+	//加载hello/about.php页面的内容
+	echo $this->render('about',array('v_hello'=>'hello_str'));
+?>

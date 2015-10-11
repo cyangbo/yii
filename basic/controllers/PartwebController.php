60dcadb848b60d087bb2849ea8ea79a4abf3134b
cyangbo <Administrator@PC-201506290051>
2015-10-11 23:18:41 +0800

    整个页面缓存

--- basic/controllers/PartwebController.php
+++ basic/controllers/PartwebController.php
@@ -22,6 +22,42 @@ class PartwebController extends Controller
 		return $this->renderPartial('index');
         
     }
+    
+    
+    /**
+     * 整个页面缓存
+     * behaviors()方法的使用
+     * 例如,访问:
+     * http://yii.com/index.php?r=partweb/index
+     * 会先执行这个控制器的behaviors()这个方法,再执行index这个方法
+     * 
+     * (non-PHPdoc)
+     * @see \yii\base\Component::behaviors()
+     */
+    public function behaviors(){
+    	
+    	echo '1';
+    	
+    	return[
+    		[
+    				'class'=>'yii\filters\PageCache',		//这样就会缓存整个index里面的页面内容
+    				'duration'=>1000,		//缓存时间1000秒
+    				'only'=>['idnex'],		//只缓存actionIndex方法操作的缓存,下面actionTest()方法不缓存
+    				'dependency'=>[			//文件依赖缓存
+    						'class'=>'yii\caching\FileDependency',
+    						'fileName'=>'hw2.txt'		//web目录下的hw2.txt
+    				]
+    		]
+    			
+    	];
+    	
+    }
+    
+    public function actionTest(){
+    	
+    	echo '测试整个页面缓存';
+    	
+    }
 
     
 }

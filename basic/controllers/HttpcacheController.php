04c0cb49e3e997be18beb71ae6e38836f699b755
cyangbo <Administrator@PC-201506290051>
2015-10-12 23:45:30 +0800

    http缓存

---
+++ basic/controllers/HttpcacheController.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * http缓存
+ */
+namespace app\controllers;
+use yii\web\Controller;
+use app\models\Test;
+use app\models\Customer;
+use app\models\Order;
+
+class HttpcacheController extends Controller
+{
+    
+	/**
+	 * http://yii.com/index.php?r=httpcache/index
+	 * 在视图文件中的内容为this is a page!
+	 * 第一次访问上面url,会缓存到,
+	 * 修改视图文件内容
+	 * 在访问一次,会显示原来的内容,修改的不被显示
+	 * 
+	 * 浏览器会缓存
+	 * 
+	 * 
+	 */
+    public function actionIndex()
+    {
+    	
+		return $this->renderPartial('idnex');
+        
+    }
+    
+    public function behaviors(){
+    	return [
+    			[
+    					'class'=>'yii\filters\HttpCache',
+    					'lastModified'=>function(){
+    						return 13322233345;
+    					}
+    					
+    			]	
+    	];
+    }
+    
+    
+    
+    
+    
+}
